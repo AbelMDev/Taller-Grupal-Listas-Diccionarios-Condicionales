2521ed6a8ac2b3cb28a8eba9a4e32b32f1dbf510
CristianUIP <cristian.pinzon@uip.edu.pa>
2025-11-05 14:50:21 -0500

    Se agregaron 2 nuevos nombres a la lista y al diccionario.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-nombres = ["abel", "luis", "jose", "carlos", "sterben"]
+nombres = ["abel", "luis", "jose", "carlos", "sterben", "pedro","noemi"]
 
 edades = {
     "abel": 28,
     "luis": 25,
     "jose": 28,
     "carlos": 35,
-    "sterben": 22
+    "sterben": 22,
+    "pedro":50,
+    "noemi":47
 }
 
 continuar = "s"
